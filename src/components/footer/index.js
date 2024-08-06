de841858f095e95ac248492e8736c7e7631defc2
kamchatskiy <kamchatskiymma@gmail.com>
2024-08-06 16:02:32 +0300

    add footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,15 +1,55 @@
-import React from 'react';
-import { FooterContainer, FooterButton } from './style';
+import * as React from "react";
+import { BottomNavigation, BottomNavigationAction } from "@mui/material";
+import { Link, useLocation } from "react-router-dom";
+import StarIcon from "@mui/icons-material/Star";
+import QrCodeIcon from "@mui/icons-material/QrCode";
+import ChecklistIcon from "@mui/icons-material/Checklist";
 
 export const Footer = () => {
+  const location = useLocation();
+  const [value, setValue] = React.useState(location.pathname);
+
+  React.useEffect(() => {
+    setValue(location.pathname);
+  }, [location]);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <FooterContainer>
-      <FooterButton variant="contained" color="primary">
-        Rating
-      </FooterButton>
-      <FooterButton variant="outlined" color="primary">
-        Tasks
-      </FooterButton>
-    </FooterContainer>
+    <BottomNavigation
+      sx={{
+        width: "100%",
+        position: "fixed",
+        bottom: 0,
+        left: 0,
+        right: 0,
+      }}
+      value={value}
+      onChange={handleChange}
+    >
+      <BottomNavigationAction
+        label="Rating"
+        value="/rating"
+        icon={<StarIcon />}
+        component={Link}
+        to="/rating"
+      />
+      <BottomNavigationAction
+        label="Tasks"
+        value="/tasks"
+        icon={<ChecklistIcon />}
+        component={Link}
+        to="/tasks"
+      />
+      <BottomNavigationAction
+        label="Scan"
+        value="/scan"
+        icon={<QrCodeIcon />}
+        component={Link}
+        to="/scan"
+      />
+    </BottomNavigation>
   );
 };
