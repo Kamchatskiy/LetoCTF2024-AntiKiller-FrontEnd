98a459bea7eb3fc946f10a84d2d1916eb6eb81cd
kamchatskiy <kamchatskiymma@gmail.com>
2024-08-06 16:03:32 +0300

    apply theme

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App";
+import { ThemeProvider } from "@mui/material/styles";
+import { theme } from "./theme";
+import { Box } from "@mui/material";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <Box>
+        <App />
+      </Box>
+    </ThemeProvider>
   </React.StrictMode>
 );
